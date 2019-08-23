dd82da297cbcfa5763aa882490b8f7c2820b7f9b
Cameron Olivier <cameronolivier@gmail.com>
2019-08-23 11:48:29 +0200

    chore: adding prettier, babel and metro config

---
+++ metro.config.js
@@ -0,0 +1,33 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const path = require('path')
+
+module.exports = {
+    projectRoot: path.resolve(__dirname, '.'),
+    watchFolders: [
+        path.resolve(__dirname),
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, 'src', 'config'),
+    ],
+    resolver: {
+        // https://github.com/facebook/metro/issues/1#issuecomment-453450709
+        extraNodeModules: new Proxy(
+            {},
+            {
+                get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
+            },
+        ),
+    },
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: false,
+            },
+        }),
+    },
+}
