a2285271715bea95bb1285d2fdf606a245d0a9ec
Jonathan Arnold <42996940+jon-nona@users.noreply.github.com>
2019-08-30 09:34:35 +0200

    chore: add jest (#1)

--- metro.config.js
+++ metro.config.js
@@ -4,24 +4,7 @@
  *
  * @format
  */
-const path = require('path')
-
 module.exports = {
-    projectRoot: path.resolve(__dirname, '.'),
-    watchFolders: [
-        path.resolve(__dirname),
-        path.resolve(__dirname, 'node_modules'),
-        path.resolve(__dirname, 'src', 'config'),
-    ],
-    resolver: {
-        // https://github.com/facebook/metro/issues/1#issuecomment-453450709
-        extraNodeModules: new Proxy(
-            {},
-            {
-                get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
-            },
-        ),
-    },
     transformer: {
         getTransformOptions: async () => ({
             transform: {
